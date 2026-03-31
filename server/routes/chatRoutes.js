77bef6eaf7b97464102872ae50ec8f0f393a5afe
abhigya <iamabhigyasingh.kccitm@zohomail.in>
2026-03-31 19:31:57 +0530

    solving integration error

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,19 +2,28 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/auth");
 const {
-  getChats,
-  getMessages,
-  sendMessage,
-  createGroup
-} = require("../controllers/chatcontroller");  // ✅ Capital C
+  getMyChats,
+  getAllUsers,
+  createOrGetChat,
+  createGroupChat,
+  addMember,
+  removeMember,
+  deleteGroup,
+  getMessages
+} = require("../controllers/chatcontroller");
 
 // All routes are protected
 router.use(protect);
 
 // Chat routes
-router.get("/", getChats);
+router.get("/", getMyChats);
+router.get("/users", getAllUsers);
+router.post("/", createOrGetChat);
+router.post("/group", createGroupChat);
+router.put("/group/:chatId/add", addMember);
+router.delete("/group/:chatId/remove/:userId", removeMember);
+router.delete("/group/:chatId", deleteGroup);
 router.get("/:chatId/messages", getMessages);
 router.post("/message", sendMessage);
-router.post("/group", createGroup);
 
 module.exports = router;
