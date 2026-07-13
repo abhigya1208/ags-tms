2b6eb60ee1f7563be5b0d4bc4ed1b837abab72da
abhigya <iamabhigyasingh.kccitm@zohomail.in>
2026-07-13 07:19:26 +0530

    updating backend

--- server/server.js
+++ server/server.js
@@ -97,10 +97,11 @@ app.use((err, req, res, next) => {
 require("./socket/chatSocket")(io);
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI || process.env.DATABASE_URL || 'mongodb://localhost:27017/ags-tms';
 
 // DATABASE & START
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
     server.listen(PORT, '0.0.0.0', () => {
